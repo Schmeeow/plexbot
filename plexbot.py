81664e98d869842af98c5e0c10e7159df56fe8a8
Schmeeow <43118081+Schmeeow@users.noreply.github.com>
2024-03-11 16:48:31 +0300

    Update plexbot.py

--- plexbot.py
+++ plexbot.py
@@ -1,146 +1,151 @@
-import telebot
-import xml.etree.ElementTree as ET
+"""
+SimPlexBot
+"""
+
+import xml.etree.ElementTree
 import urllib.request
-import datetime
 import time
+import telebot
 
-### CONFIG
+###  НАСТРОЙКИ
 
-BOT_TOKEN = ''  # токен бота
+BOT_TOKEN = '' # TG токен бота
 
-ALLOWED_USERS = (11111111,22222222,3333333)  #список разрешенных пользователей (telegram.user.id)
+ALLOWED_USERS = (11111111,222222222,3333333333) # TG ID разрешенных пользователей
 
-# настройки сервера и библиотек. XML-файлы библиотек обычно лежат по адресу вида 'http://SERVER_IP:32400/library/sections/ID/all, ID обычно идут по порядку. поддерживается только два вида медиа - Movies и Series
+# Настройки сервера и библиотек. XML-файлы библиотек обычно лежат по адресу
+# вида 'http://SERVER_IP:32400/library/sections/ID/all, ID обычно идут
+# по порядку. поддерживается только два вида медиа - Movies и Series
 
-PLEX_SERVER_CONFIG = { 'url':'http://192.168.1.111',   
+PLEX_SERVER_CONFIG = { 'url':'http://192.168.1.111',
                        'port':'32400',
-                       'libraries':[{
-                                     'name' : 'ФИЛЬМЫ',
-                                     'type' : 'movies',
-                                     'id' : '1'
-                                    },
-                                    {
-                                     'name' : 'СEРИАЛЫ',
-                                     'type' : 'series',
-                                     'id' : '2'
-                                    },
-                                    {
-                                     'name' : 'МУЛЬФИЛЬМЫ',
-                                     'type' : 'movies',
-                                     'id' : '3'
-                                    },
-                                    {
-                                     'name' : 'МУЛЬТСЕРИАЛЫ',
-                                     'type' : 'series',
-                                     'id' : '4'
-                                    },
-                                    {
-                                     'name' : 'ДОКУМЕНТАЛКИ',
-                                     'type' : 'series',
-                                     'id' : '8'}]
-                                    }
+                       'libraries':[{'name':'ФИЛЬМЫ','id':'1'},
+                                    {'name':'СEРИАЛЫ','id':'2'},
+                                    {'name':'МУЛЬТФИЛЬМЫ','id':'3'},
+                                    {'name':'МУЛЬТСЕРИАЛЫ','id':'4'},
+                                    {'name':'ДОКФИЛЬМЫ','id':'10'},
+                                    {'name':'ДОКСЕРИАЛЫ','id':'8'}] }
 
 FRESH_DAYS = 14  #сколько дней должно пройти, чтобы медиа перестало считаться новым
 
-### FUNCTIONS
+### ФУНКЦИИ
 
-libRoots = {}
+lib_roots = {}
 
-def load_and_parse ():
+def load_and_parse() -> dict:
+    """ Грузим XML и преобразуем в деревья """
     for library in PLEX_SERVER_CONFIG['libraries']:
-        url = PLEX_SERVER_CONFIG['url'] + ":" + PLEX_SERVER_CONFIG['port'] + "/library/sections/" + library['id'] + "/all"
-        urlHandle = urllib.request.urlopen(url)
-        parsedHandle = ET.parse(urlHandle)
-        libRoots[str(library['name'])] = parsedHandle.getroot()
+        url = f"{PLEX_SERVER_CONFIG['url']}:{PLEX_SERVER_CONFIG['port']}" \
+              f"/library/sections/{library['id']}/all"
+        with urllib.request.urlopen(url) as url_handle:
+            parsed_handle = xml.etree.ElementTree.parse(url_handle)
+        lib_roots[library['name']] = parsed_handle.getroot()
 
-def split_str(seq, chunk, skip_tail=False):
-    lst = []
-    if chunk <= len(seq):
-        lst.extend([seq[:chunk]])
-        lst.extend(split_str(seq[chunk:], chunk, skip_tail))
-    elif not skip_tail and seq:
-        lst.extend([seq])
-    return lst
-
-def get_all(freshness=10000):
-    i = 0
-    replyString = ""
-    freshness = freshness*24*60*60
-    for lib in libRoots:
-        library = PLEX_SERVER_CONFIG['libraries'][i]
-        replyString = replyString + "\n" + library['name']  + ":" + "\n"
-        if library['type'] == "movies":
-           counter = 0
-           for child in libRoots[lib].iter('Video'):
-              if (int(time.time()) - freshness) <= int(child.attrib['addedAt']):
-                 counter += 1
-                 replyString = replyString + str(counter) + '. ' + child.attrib['title'] + ' (' + child.attrib['year'] + ')' + "\n"
-        else:
-           for child in libRoots[lib].iter('Directory'):
-              if (int(time.time()) - freshness) <= int(child.attrib['addedAt']):
-                 counter += 1
-                 replyString = replyString + str(counter) + '. ' + child.attrib['title'] + ' (' +  child.attrib['childCount'] + ' сезонов, '  + child.attrib['leafCount']+ ' серий)' + "\n"
-        if counter == 0:
-                 replyString = replyString + 'Ничего не найдено\n'
-        i += 1
-        counter = 0
-    return(replyString)
-
-def search_by_string(message):
+def get_list(freshness = 10000) -> list:
+    """ Получаем из библиотек список элементов с заданной свежестью.
+    По умолчанию свежесть равна 10000 дням, то есть выводятся все элементы.
+    Пустые библиотеки не выводятся """
     load_and_parse()
-    searchString = message.text.replace('ё', 'е')
-    i = 0
-    replyString = ""
-    for lib in libRoots:
-        library = PLEX_SERVER_CONFIG['libraries'][i]
-        replyString = replyString + "\n" + library['name']  + ":" + "\n"
-        if library['type'] == "movies":
-           counter = 0
-           for child in libRoots[lib].iter('Video'):
-              if searchString.casefold() in child.attrib['title'].replace('ё', 'е').casefold():
-                 counter += 1
-                 replyString = replyString + str(counter) + '. ' + child.attrib['title'] + ' (' + child.attrib['year'] + ')' + "\n"
-        else:
-           for child in libRoots[lib].iter('Directory'):
-              if searchString.casefold() in child.attrib['title'].replace('ё', 'е').casefold():
-                 counter += 1
-                 replyString = replyString + str(counter) + '. ' + child.attrib['title'] + ' (' +  child.attrib['childCount'] + ' сезонов, '  + child.attrib['leafCount']+ ' серий)' + "\n"
-        if counter == 0:
-                 replyString = replyString + 'Ничего не найдено\n'
-        i += 1
-        counter = 0
+    results = {}
+    freshness = freshness*24*60*60
+    best_before = time.time() - freshness
+    for lib, content in lib_roots.items():
+        items = []
+        if content.attrib['viewGroup'] == "movie":
+            for child in content.iter('Video'):
+                if int(child.attrib['addedAt']) >= best_before:
+                    item_desc = f"{child.attrib['title']} ({child.attrib['year']})"
+                    items.append(item_desc)
+        elif content.attrib['viewGroup'] == "show":
+            for child in content.iter('Directory'):
+                if int(child.attrib['addedAt']) >= best_before:
+                    item_desc = f"{child.attrib['title']} ({child.attrib['childCount']} сезонов, "\
+                                f"{child.attrib['leafCount']} серий)"
+                    items.append(item_desc)
+        if len(items) !=0:
+            results[lib] = items
+    return results
 
-    for part in split_str(replyString, 4080):
-        bot.send_message(message.chat.id, part)
+def search_by_string(message) -> list:
+    """ Получаем из библиотек список элементов с совпадениями по поисковой фразе.
+    Для пустых библиотек выводим фразу 'Ничего не найдено'. Заодно меняем е на ё """
+    load_and_parse()
+    found = False
+    results = {}
+    search_string = message.text.replace('ё', 'е')
+    for lib, content in lib_roots.items():
+        items = []
+        if content.attrib['viewGroup'] == "movie":
+            for child in content.iter('Video'):
+                if search_string.casefold() in child.attrib['title'].replace('ё', 'е').casefold():
+                    item_desc = f"{child.attrib['title']} ({child.attrib['year']})"
+                    items.append(item_desc)
+                    found = True
+        elif content.attrib['viewGroup'] == "show":
+            for child in content.iter('Directory'):
+                if search_string.casefold() in child.attrib['title'].replace('ё', 'е').casefold():
+                    item_desc = f"{child.attrib['title']} ({child.attrib['childCount']} сезонов, "\
+                                f"{child.attrib['leafCount']} серий)"
+                    items.append(item_desc)
+                    found = True
+        if len(items) !=0:
+            results[lib] = items
+    if found is True:
+        message_parts = compose_message(results, 100)
+        for part in message_parts:
+            bot.send_message(message.chat.id, part)
+    else:
+        bot.send_message(message.chat.id, "К сожалению, ничего не найдено")
 
-## BOT ACTIONS
+def compose_message(results, rows) -> list:
+    """ Из результатов поиска по библиотеке формируем сообщения для телеграм
+    разбивая их на части из-за ограничения на длину одного сообщения """
+    message_parts = []
+    for key in results:
+        lines = []
+        lines.append(f"{key} ({len(results[key])})\n")
+        for number, item in enumerate(results[key],1):
+            lines.append(f"{number}. {item}\n")
+        for i in range(0, len(lines), rows):
+            message_parts.append(lines[i:i+rows])
+    strings = []
+    for part in message_parts:
+        strings.append(''.join(part))
+    return strings
+
+## БОТ
 
 bot = telebot.TeleBot(BOT_TOKEN)
 
 @bot.message_handler(func=lambda message: message.chat.id not in ALLOWED_USERS)
 def auth_failed(message):
-        bot.send_message(message.chat.id, "Доступ запрещен. Попросите администратора бота добавить вас в белый лист.")
+    """ Проверка пользователя на список допущенных """
+    bot.send_message(message.chat.id, 'Доступ запрещен. Попросите добавить вас в белый лист.')
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-        bot.reply_to(message, "Алоха! Жми на кнопку МЕНЮ внизу и выбирай команду")
+    """ Обработка команды /start """
+    bot.reply_to(message, "Привет! Введи текст для поиска или выбери команду в меню")
 
 @bot.message_handler(commands=['new'])
 def list_new(message):
-        load_and_parse()
-        for part in split_str(get_all(FRESH_DAYS), 4080):
-            bot.send_message(message.chat.id, part)
+    """ Вывод новых элементов по команде /new"""
+    results = get_list(FRESH_DAYS)
+    message_parts = compose_message(results, 100)
+    for part in message_parts:
+        bot.send_message(message.chat.id, part)
 
 @bot.message_handler(commands=['list'])
 def list_all(message):
-        load_and_parse()
-        for part in split_str(get_all(), 4080):
-            bot.send_message(message.chat.id, part)
+    """ Вывод всех элементов по команде /list """
+    results = get_list()
+    message_parts = compose_message(results, 100)
+    for part in message_parts:
+        bot.send_message(message.chat.id, part)
 
 @bot.message_handler(content_types=['text'])
 def search_command(message):
-    if message.text == '/search':
-        msg = bot.send_message(message.from_user.id, 'Введите поисковый запрос: ')
-        bot.register_next_step_handler(msg, search_by_string)
+    """ Поиск по любой текстовой фразе """
+    search_by_string(message)
 
 bot.infinity_polling()
